699fc0bf175be5c16723206dd096c0a089101e34
Chekrygin Kirill <56756165+azaazazaa@users.noreply.github.com>
2024-04-15 20:25:03 +0300

    Update games.js
    Добавлены заготовки для удаления по id и добавления новой игры

--- routes/games.js
+++ routes/games.js
@@ -2,7 +2,17 @@
 const gamesRoute = require("express").Router();
 
 const getAllGames = async (req, res) => {};
+const deleteGames = async (req, res) => {
+  console.log("Удалено!");
+};
+const addGame = async (req, res) => {
+  console.log("Удалено!");
+};
 
 gamesRoute.get("/games", getAllGames);
+
+gamesRoute.delete("/games/:id", deleteGames);
+
+gamesRoute.post("/games", addGame);
 
 module.exports = gamesRoute;
