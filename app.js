9b91fdab1b8b9e81ccf7a0e3007443df806f8637
Chekrygin Kirill <56756165+azaazazaa@users.noreply.github.com>
2024-05-13 22:10:36 +0500

    Добавили создание JWT и проверку

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const apiRouter = require("./routes/api");
+const pagesRouter = require("./routes/pages");
 
-const connectToDatabase = require('./database/connect');
+const connectToDatabase = require("./database/connect");
 const cors = require("./middlewares/cors.js");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 3000;
@@ -13,9 +15,11 @@ connectToDatabase();
 
 app.use(
   cors,
+  cookieParser(),
   bodyParser.json(),
   apiRouter,
-  express.static(path.join(__dirname, "public")),
+  pagesRouter,
+  express.static(path.join(__dirname, "public"))
 );
 
 app.listen(PORT, () => {
